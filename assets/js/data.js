2108fd6d759a83e58bd3ee525a2de942b9c301ce
jfru1 <frew1@umbc.edu>
2017-06-21 20:10:02 -0400

    Add files via upload

--- assets/js/data.js
+++ assets/js/data.js
@@ -8,12 +8,6 @@ var config = {
 };
 firebase.initializeApp(config)
 
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-
-	})
-		
 var name = "";
 var role = "";
 var startDate = "";
@@ -22,35 +16,48 @@ var database = firebase.database();
 var rate = null
 var monthPaid = null
 var totalPaid = null
+var dateAdded = null
 
 $("#submit-form").on("click", function() {
-
 	event.preventDefault();
 
-	name = $("#").val().trim();
-	role = $("#").val().trim();
-	startDate = $("#").val().trim();
-	datePlus = $("#").val().trim();
-	//database = $("#").val().trim();
-	rate = $("#").val().trim();
-	monthPaid = $("#").val().trim();
-	totalPaid = $("#").val().trim();	
+	name = $("#name").val().trim();
+	role = $("#role").val().trim();
+	startDate = $("#startDate").val().trim();
+	rate = $("#salary").val().trim();
 
 	empInfo = {
 		eName:name
 		eRole:role
 		eStart:startDate
 		eRate:rate
+		dateAdded:firebase.database.ServerValue.TIMESTAMP
 	}
 	database.ref().push(empInfo)
 
-	database.ref().on("value", function(snapshot) {
-		console.log(snapshot.val())
-		console.log(snapshot.val().name)
-		console.log(snapshot.val().role)
-		console.log(snapshot.val().startDate)
-		console.log(snapshot.val().rate)
+	database.ref().on("child_added", function(childSnapshot) {
+		console.log(childSnapshot.val())
+		console.log(childSnapshot.val().name)
+		console.log(childSnapshot.val().role)
+		console.log(childSnapshot.val().startDate)
+		console.log(childSnapshot.val().rate)
+	}, function(errorObject) {
+		console.log("Error logged: " + errorObject.code);
 	})
 
+	$("#full-member-list").append("<div class='well'><span id='name'> " + 
+		childSnapshot.val().name +
+		childSnapshot.val().role +
+		childSnapshot.val().startDate +
+		childSnapshot.val().rate + 
+		//calculations go here for months worked, total pay
+		)
 
-})
+	console.log("Employee added to database")
+
+	$("#name").empty()
+	$("#role").val("")
+	$("#start-year").val("")
+	$("#salary").val("")
+});
+
